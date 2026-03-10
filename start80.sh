3309ecacbf0c0b9502836d9ca64470218ece4914
go2thanawat-web <go2thanawat@gmail.com>
2026-03-10 22:23:33 +0700

    Update start80.sh

--- start80.sh
+++ start80.sh
@@ -16,7 +16,7 @@ count=${#games[@]}
 index=0
 
 if [ $count -eq 0 ]; then
-    echo "No game disks found"
+    echo "No disk files found"
     exit
 fi
 
@@ -24,18 +24,25 @@ while true
 do
 
 clear
+
+# CRT GREEN
 echo -e "\e[32m"
 
 echo "################################################"
-echo "#     NEC PC-8001 DISK DRIVE EMULATOR         #"
+echo "#                                              #"
+echo "#     NEC PC-8001 DISK DRIVE EMULATOR          #"
+echo "#                                              #"
 echo "################################################"
 echo ""
 echo " Drive A : SDOS.d88"
 echo " Drive B : ${games[$index]}"
 echo ""
-echo " Games found : $count"
+echo " Disk $((index+1))/$count"
 echo ""
-echo " DEBUG: waiting GPIO3 button..."
+echo " GPIO3 = NEXT DISK"
+echo " GPIO2 = PREVIOUS DISK"
+echo ""
+echo " Waiting for button..."
 echo ""
 
 ./pc80s31 SDOS.d88 "${games[$index]}" &
@@ -45,21 +52,32 @@ PID=$!
 while true
 do
 
-    state=$(raspi-gpio get 3 | grep -o "level=[01]" | cut -d= -f2)
+next=$(raspi-gpio get 3 | grep -o "level=[01]" | cut -d= -f2)
+prev=$(raspi-gpio get 2 | grep -o "level=[01]" | cut -d= -f2)
 
-    echo -ne "GPIO3 level = $state   \r"
+echo -ne "GPIO3=$next  GPIO2=$prev   \r"
 
-    if [ "$state" = "0" ]; then
-        echo ""
-        echo "BUTTON DETECTED!"
-        break
-    fi
+if [ "$next" = "0" ]; then
+    echo ""
+    echo "NEXT DISK SELECTED"
+    index=$(( (index+1) % count ))
+    break
+fi
 
-    sleep 0.1
+if [ "$prev" = "0" ]; then
+    echo ""
+    echo "PREVIOUS DISK SELECTED"
+    index=$(( (index-1+count) % count ))
+    break
+fi
+
+sleep 0.1
 
 done
 
+echo ""
 echo "Stopping emulator..."
+
 kill $PID 2>/dev/null
 sleep 1
 
@@ -71,6 +89,5 @@ fi
 
 echo "Switching disk..."
 sleep 1
-index=$(( (index+1) % count ))
 
 done
